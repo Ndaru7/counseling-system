6c34a3a01fdace9374330568bb8d58a1b823b071
Ndaru <daruprasetiyo123@gmail.com>
2025-10-09 12:23:19 +0700

    tambah fitur notifikasi dengan whatsapp ke orang tua/wali

--- catatan_konseling/aksi.php
+++ catatan_konseling/aksi.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 require_once "../database/config.php";
+require_once "../bot.php";
 
 
 if (isset($_POST["simpan"])) {
@@ -17,14 +18,22 @@ if (isset($_POST["simpan"])) {
                                                                 '$pelanggaran',
                                                                 '$deskripsi',
                                                                 '$pencatat')");
-        $query_poin = "SELECT poin FROM tb_siswa WHERE nisn = '$siswa' ";
-        $query_pelanggaran = "SELECT poin FROM tb_pelanggaran WHERE id = '$pelanggaran' ";
+        $query_poin = "SELECT nama, no_hp, poin FROM tb_siswa WHERE nisn = '$siswa' ";
+        $query_pelanggaran = "SELECT kategori, poin FROM tb_pelanggaran WHERE id = '$pelanggaran' ";
         $row_poin = pdo_query($conn, $query_poin)->fetch(PDO::FETCH_ASSOC);
         $row_pelanggaran = pdo_query($conn, $query_pelanggaran)->fetch(PDO::FETCH_ASSOC);
+        $nama_siswa = $row_poin["nama"];
         $poin_siswa = $row_poin["poin"];
+        $no_hp_ortu = $row_poin["no_hp"];
         $poin_pelanggaran = $row_pelanggaran["poin"];
+        $kategori_pelanggaran = $row_pelanggaran["kategori"];
         $query_poin = pdo_query($conn, "UPDATE tb_siswa SET poin = ('$poin_siswa' + '$poin_pelanggaran') WHERE nisn = '$siswa' ");
         $berhasil++;
+
+        //Kirim notifikasi ke orang tua
+        $pesan = pesanKonseling($siswa, $nama_siswa, date("Y-m-d H:i:s"), $kategori_pelanggaran, $deskripsi, $pencatat);
+        kirimPesan($no_hp_ortu, $pesan);
+        
     }
     $_SESSION["flash"] = [
         "type" => "success",
